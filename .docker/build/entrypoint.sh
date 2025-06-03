f324e62c0cc237f3f14162113a01bcbdc0ec9a3a
Luan Santos <lvluansantos@gmail.com>
2025-06-03 16:04:53 -0300

    DEV-144 Fix:  corrigido load do entrypoint.

--- .docker/build/entrypoint.sh
+++ .docker/build/entrypoint.sh
@@ -16,13 +16,15 @@ PHP_UPLOAD_MAX_FILESIZE=${PHP_UPLOAD_MAX_FILESIZE:-2G}
 PHP_MAX_FILE_UPLOADS=${PHP_MAX_FILE_UPLOADS:-20}
 PHP_MEMORY_LIMIT=${PHP_MEMORY_LIMIT:--1}
 
-# Insert values in custom php ini.
-echo "max_execution_time = ${PHP_MAX_EXECUTION_TIME}" >> /usr/local/etc/php/conf.d/1-custon.ini
-echo "max_input_time = ${PHP_MAX_INPUT_TIME}" >> /usr/local/etc/php/conf.d/1-custon.ini
-echo "post_max_size = ${PHP_POST_MAX_SIZE}" >> /usr/local/etc/php/conf.d/1-custon.ini
-echo "upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}" >> /usr/local/etc/php/conf.d/1-custon.ini
-echo "max_file_uploads = ${PHP_MAX_FILE_UPLOADS}" >> /usr/local/etc/php/conf.d/1-custon.ini
-echo "memory_limit = ${PHP_MEMORY_LIMIT}" >> /usr/local/etc/php/conf.d/1-custon.ini
+# Use ambos para CLI e FPM
+for target in /etc/php/8.4/cli/conf.d/99-custom.ini /etc/php/8.4/fpm/conf.d/99-custom.ini; do
+    echo "max_execution_time = ${PHP_MAX_EXECUTION_TIME}" > $target
+    echo "max_input_time = ${PHP_MAX_INPUT_TIME}" >> $target
+    echo "post_max_size = ${PHP_POST_MAX_SIZE}" >> $target
+    echo "upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}" >> $target
+    echo "max_file_uploads = ${PHP_MAX_FILE_UPLOADS}" >> $target
+    echo "memory_limit = ${PHP_MEMORY_LIMIT}" >> $target
+done
 
 
 # Altera o APP_URL diretamente no .env
